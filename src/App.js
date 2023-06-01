3426af88d7a59e844c22f0e14d56a00a71ee7e83
alexandre xu <alexandre.xu.mail@gmail.com>
2023-06-01 16:47:17 +0200

    Commit2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+// App.js
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import data from './data.json';
+import Apartment from './Apartment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div className="App-content">
+          {data.map((apartment) => (
+            <Apartment key={apartment.id} title={apartment.title} description={apartment.description} />
+          ))}
+        </div>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>Hello, this is a test !</div>
+    );
+  }
+}
+
+export default MyComponent;
